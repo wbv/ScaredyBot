f62e9b4c21f1707b4c1e194b9d1fc15f6447b286
GraceAndersen <supergracer1@gmail.com>
2016-11-23 11:44:42 -0700

    Made everything pretty

--- ScaredyBot.ino
+++ ScaredyBot.ino
@@ -5,6 +5,7 @@
 #define RIGHT_MOTOR_1A2A 5
 #define RIGHT_MOTOR_1A 10
 #define RIGHT_MOTOR_2A 9
+#define VELOCITY 100
 
 bool serial_begun = false;
 
@@ -38,106 +39,56 @@ void loop() {
 
 if (distance_cm(SFRONT) < 10.0 && distance_cm(SFRONT) != 0){
    //backwards
-   Serial.println("SFRONT < 10.0");
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("backwards");
+   backwards(500, VELOCITY);
    delay(49);
 }   
 else if (distance_cm(SFRONT_L) < 10.0 && distance_cm(SFRONT_L) != 0){
    //turn 45 CC
-   Serial.println("SFRONT_L < 10.0");
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(125);
-   //Go backwards
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("turn 45 CC and go backwards");
+   counter_clocwise(125, VELOCITY);
+   backwards(500, VELOCITY);
    delay(49);
 }   
 else if (distance_cm(SLEFT) < 10.0 && distance_cm(SLEFT) != 0){
    //turn 90 CC
-   Serial.println("SLEFT < 10.0");
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(250);
-   //Go backwards
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("turn 90 cc and go backwards");
+   counter_clockwise(250, VELOCITY);
+   backwards(500, VELOCITY);
    delay(49);
 }   
 else if (distance_cm(SBACK_L) < 10.0 && distance_cm(SBACK_L) != 0){
    //turn 45 C
-   Serial.println("SBACK_L < 10.0");
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(125);
-   //Go forwards
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("45 C forwards");
+   clockwise(125, VELOCITY);
+   forwards(500, VELOCITY);
    delay(49);
 }   
 else if (distance_cm(SBACK) < 10.0 && distance_cm(SBACK) != 0){
    //go forward
-   Serial.println("SBACK < 10.0");
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("forwards");
+   forwards(500, VELOCITY);  
    delay(49);
 }  
 else if (distance_cm(SBACK_R) < 10.0 && distance_cm(SBACK_R) != 0){
    //turn 45 CC
-   Serial.println("SBACK_R < 10.0");
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(125);
-   //go forward
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("45 CC and forward");
+   counter_clockwise(125, VELOCITY);
+   forward(500, VELOCITY);
    delay(49);
 }   
 else if (distance_cm(SRIGHT) < 10.0 && distance_cm(SRIGHT) != 0){
    //turn 90 CC
-   Serial.println("SRIGHT < 10.0");
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(250);
-   //go forward
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, 100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("90 CC forwards");
+   counter_clockwise(250, VELOCITY);
+   forwards(500, VELOCITY);
    delay(49);
 }  
 else if (distance_cm(SFRONT_R) < 10.0 && distance_cm(SFRONT_R) != 0){
    // turn 45 C
-   Serial.println("SFRONT_R < 10.0");
-   motor_speed(LEFT_MOTOR, 100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(125);
-   //go backward
-   motor_speed(LEFT_MOTOR, -100);
-   motor_speed(RIGHT_MOTOR, -100);
-   delay(400);
-   motor_speed(LEFT_MOTOR, 0);
-   motor_speed(RIGHT_MOTOR, 0);
+   Serial.println("45 C backwards");
+   clockwise(125, VELOCITY);
+   backwards(500, VELOCITY);
    delay(49);
 }   
 else {
